93dfc396695a3b288b4ab6b8c6f48f6fd3bc99ff
mobyw <mobyw66@gmail.com>
2024-02-19 17:12:34 +0800

    :sparkles: Update default github proxy

--- march7th/nonebot_plugin_srres/config.py
+++ march7th/nonebot_plugin_srres/config.py
@@ -5,7 +5,7 @@ from pydantic import Extra, BaseModel
 
 
 class Config(BaseModel, extra=Extra.ignore):
-    github_proxy: Optional[str] = "https://ghproxy.com"
+    github_proxy: Optional[str] = "https://mirror.ghproxy.com"
     sr_wiki_url: Optional[
         str
     ] = "https://raw.githubusercontent.com/Mar-7th/StarRailRes/master"
