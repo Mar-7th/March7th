8f47bf4192e16f13364b6882e08efd904be0ab69
mobyw <mobyw66@gmail.com>
2024-03-31 01:12:35 +0800

    :bug: Fix default github proxy #38

--- march7th/nonebot_plugin_srpanel/config.py
+++ march7th/nonebot_plugin_srpanel/config.py
@@ -6,7 +6,7 @@ from pydantic import Extra, BaseModel
 
 class Config(BaseModel, extra=Extra.ignore):
     sr_panel_url: Optional[str] = "https://api.mihomo.me/sr_info_parsed/"
-    github_proxy: Optional[str] = "https://ghproxy.com"
+    github_proxy: Optional[str] = "https://mirror.ghproxy.com"
     sr_score_url: Optional[
         str
     ] = "https://raw.githubusercontent.com/Mar-7th/StarRailScore/master/score.json"
